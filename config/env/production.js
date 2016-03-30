a053e4fdd18d7eb1a9cf3fc57387ee150286569e
dlfinis <dln.finis@gmail.com>
2016-03-30 15:35:37 -0500

    Add openshift configuration

--- config/env/production.js
+++ config/env/production.js
@@ -63,8 +63,10 @@ module.exports = {
     /***************************************************************************
      * Set the port in the production environment to 80                        *
      ***************************************************************************/
-
-     port: process.env.PORT || 1337
+     host: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
+     port: process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080,
+     environment: process.env.NODE_ENV || 'development',
+    //  port: process.env.PORT || 1337,
 
     /***************************************************************************
      * Set the log level in production environment to "silent"                 *
