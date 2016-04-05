d8bfbe6abceb6cdb7b07cfc9d505a8966a520717
dlfinis <dln.finis@gmail.com>
2016-04-05 02:36:09 -0500

    Heroku Deploy

--- config/env/production.js
+++ config/env/production.js
@@ -74,7 +74,7 @@ module.exports = {
     /***************************************************************************
      * Set the port in the production environment to 80                        *
      ***************************************************************************/
-     host: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
+    //  host: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
 
     //  port: process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080,
      port: process.env.PORT || 1337,
