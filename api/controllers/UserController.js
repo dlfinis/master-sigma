aaee86a1c8ea07b53d4b70623c11d171cedc8e01
DLFinis <dln.finis@gmail.com>
2016-10-11 13:54:32 -0500

    Edit add-content component

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -8,10 +8,12 @@
 module.exports = {
   me: function(req, res) {
     if ( !req.isAuthenticated() ) return res.json(403,{});
+    console.log(req.session.user);
     return res.json({ user: {
       id: req.session.user.id,
       uid: req.session.user.uid,
       name: req.session.user.name,
+      state: req.session.user.state,
       origin: req.session.origin
     }
     });
