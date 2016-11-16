d175b68bd4fd3ad6643792da37e1d89227560dd8
DLFinis <dln.finis@gmail.com>
2016-11-16 00:36:01 -0500

    Update assets

--- assets/angular/app.fb.config.js
+++ assets/angular/app.fb.config.js
@@ -1,12 +1,11 @@
  (function () { 
 'use strict';
 
- angular.module('app.config', [])
+ angular.module('app.fb.config', [])
 
-.constant('FB', {clientID:'1267766483237355',permissions:'email,user_birthday,user_friends,publish_actions'})
+.constant('FB', {clientID:{general:1267766483237355,zeus:1545700372110630,ares:1545715552109112,apolo:1545716165442384},permissions:'email,user_birthday,user_friends,publish_actions'})
 
 .value('debug', false)
 
 ;
-
 })();
